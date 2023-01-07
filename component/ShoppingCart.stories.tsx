22b7f76e1200d2f7ff1540617fe00ca7b353fb4f
Nahuel Rabey <nahuelrabeywork@gmail.com>
2023-01-07 05:28:24 -0300

    clearer

--- component/ShoppingCart.stories.tsx
+++ component/ShoppingCart.stories.tsx
@@ -1,8 +1,9 @@
-import {OldShoppingCart} from "./ShoppingCart"
+import {ShoppingCart} from "./ShoppingCart"
 import {ComponentStory, ComponentMeta} from "@storybook/react"
 
 export default {
-    component: OldShoppingCart
-} as ComponentMeta<typeof OldShoppingCart>
+    component: ShoppingCart
+} as ComponentMeta<typeof ShoppingCart>
 
-export const Primary: ComponentStory<typeof OldShoppingCart> = () => <OldShoppingCart/>
+export const Unactive: ComponentStory<typeof ShoppingCart> = () => <ShoppingCart/>
+export const Active: ComponentStory<typeof ShoppingCart> = () => <ShoppingCart count={1}/>
--- component/ShoppingCart.tsx
+++ component/ShoppingCart.tsx
@@ -1,45 +1,39 @@
 /* eslint-disable @next/next/no-img-element */
-import React, { useEffect, useState } from "react"
-import Image from "next/image"
+import { useEffect } from "react"
+import React from "react"
 import style from "./ShoppingCart.module.css"
 
 type args = { count?: number }
 
 export function ShoppingCart({ count = 0 }: args) {
-    const [svgSize, setSize] = useState(0)
-    const [sizePadding, setPadding] = useState(0)
-    const [textSize, setTextSize] = useState(0)
-
     const isActive = count > 0
-    // const counterRef = React.createRef<HTMLDivElement>()
     const textRef = React.createRef<HTMLSpanElement>()
+    const imRef = React.createRef<HTMLImageElement>()
+    const divRef = React.createRef<HTMLDivElement>()
+    const circleClassName = `${isActive ? style.active : ""} ${style.circle}`
     useEffect(() => {
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        const size = Math.max( window.innerWidth / 40, 30)
-        setSize(size)
-        setTextSize(textRef.current?.offsetWidth || 0)
-        setPadding((size - textSize)/2)
+        if (!textRef.current || !imRef.current || !divRef.current) {
+            return
+        }
 
-        console.table({svgSize, sizePadding, textSize})
-    }, [sizePadding, svgSize, textRef, textSize])
+        const radius = imRef.current.offsetWidth * 0.65;
+        const textWidth = textRef.current.offsetWidth
+
+        // padding to be set to the div containig span
+        const sidePadding = (radius - textWidth) / 2;
+        divRef.current.style.paddingLeft = `${sidePadding}px`;
+        divRef.current.style.paddingRight = `${sidePadding}px`;
+        divRef.current.style.height = `${radius}px`;
+
+    })
     return (
         <div className={style.cart}>
-            <Image src="/shopping-cart.svg" alt="shopping cart" width={svgSize} height={svgSize} />
-            <div className={isActive ? "active" : ""} style={{ paddingLeft: sizePadding, paddingRight: sizePadding }}>
-                <span ref={textRef}>{count}</span>
+            <img src="/shopping-cart.svg" alt="shopping cart" ref={imRef} />
+            <div>
+                <div className={circleClassName} ref={divRef}>
+                    <span ref={textRef}>{count}</span>
+                </div>
             </div>
         </div>
     )
 }
-
-// export function ShoppingCart({count = 0}: args){
-//     const isActive = count > 0
-//     return (
-//         <div className={style.cart}>
-//             <img src="/shopping-cart.svg" alt="shopping cart"/>
-//             <div className={isActive ? "active" : ""}>
-//                 <span>{count}</span>
-//             </div>
-//         </div>
-//     )
-// }
